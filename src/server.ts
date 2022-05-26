650d9e880451b5451c1e407fd109f30f7fa65eed
Rafinhaa <rafinhaa@me.com>
2022-05-26 09:37:59 -0300

    feat: Implemented send mail

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from "express";
 import { prisma } from "./prisma";
+import nodemailer from "nodemailer";
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "07cf92621d262c",
+    pass: "deb58a126d7c76",
+  },
+});
 
 app.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
@@ -15,6 +25,21 @@ app.post("/feedbacks", async (req, res) => {
       screenshot,
     },
   });
+  await transport.sendMail({
+    from: "Equipe FeedGet <oi@feedget.com>",
+    to: "Rafael Rodrigues <rafael@abc.com.br",
+    subject: "Feedback do usuário",
+    text: "Um novo feedback foi recebido!",
+    html: [
+      `<h3>Novo feedback</h3>`,
+      `<ul>`,
+      `<li>Tipo: ${feedback.type}</li>`,
+      `<li>Comentário: ${feedback.comment}</li>`,
+      `<li>Screenshot: ${feedback.screenshot}</li>`,
+      `</ul>`,
+    ].join("\n"),
+  });
+
   return res.status(201).json({ data: feedback });
 });
 
